720c626fb1432fd791a51363b7f56151592bf7c9
FJ-WYF <1228672908@qq.com>
2021-02-21 13:23:21 +0800

    add

---
+++ 函数递归求n的k次方.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//函数递归求n的k次方
+//float nk(int n,int k)
+//{
+//	if (k == 0)
+//		return 1;
+//	else if (k > 0)
+//		return n * nk(n, k - 1);
+//	else
+//		return (1.0/n)*(nk(n,k+1));
+//}
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	scanf("%d%d", &n,&k);
+//	float outcome = nk(n,k);
+//	printf("%f", outcome);
+//	return 0;
+//}
