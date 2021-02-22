783f4a3af4b1bef502f1f65b6b48d811b38affb5
FJ-WYF <1228672908@qq.com>
2021-02-22 09:57:09 +0800

    add

---
+++ 宏offsetof的实现.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+struct S {
+	char a;
+	int b;
+	char c;
+};
+#define OFFSETOF(struct_name,member_name) (int)&(((struct_name*)0)->member_name)
+int main()
+{
+	printf("%d\n", OFFSETOF(struct S, a));
+	printf("%d\n", OFFSETOF(struct S, b));
+	printf("%d\n", OFFSETOF(struct S, c));
+	return 0;
+}
